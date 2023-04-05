028c19b285de0dd735295845870d5efbfd035f21
Fardowsamohamed <muniiramohamed096@gmail.com>
2023-04-05 16:46:37 +0300

    second commit

--- InputOutputStream.java
+++ InputOutputStream.java
@@ -1,23 +1,56 @@
 package file;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class InputOutputStream {
     public static void main(String[] args) throws IOException {
         FileInputStream in =null;
         FileOutputStream out =null;
         try{
+            File obj =new File("input.txt");
+            File Obj=new File("output.txt");
+
+            FileWriter myFile=new FileWriter("input.txt");
+            myFile.write("Files in Java might be tricky, but it is fun enough");
+            myFile.close();
+            System.out.println("the file successfully done:");
+
+            FileWriter MyFile=new FileWriter("output.txt");
+            MyFile.write("To create a file in a specific directory");
+            MyFile.close();
+            System.out.println("the file successfully done");
+
+
+            if(obj.createNewFile()){
+                System.out.println("File created "+obj.getName());
+            }else{
+                System.out.println("the file already exists");
+            }
+
+            if(Obj.createNewFile()){
+                System.out.println("file created: "+ Obj.getName());
+            }else{
+                System.out.println("The file already exists");
+            }
+
+
             in= new FileInputStream("input.txt");
             out=new FileOutputStream("output.txt");
+
             int c;
             while((c=in.read())!= -1){
                 out.write(c);
+               // System.out.println("file already exists");
             }
-        }finally {
+
+
+        }catch(IOException e){
+            System.out.println("error occurs");
+        }
+        finally {
             if(in !=null){
                 in.close();
+
             }
             if(out !=null){
                 out.close();
